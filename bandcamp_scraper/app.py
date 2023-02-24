4be40641cb5a8f8444feb27783ab780b3ced3803
Maxim <osipovs.maxim@gmail.com>
2023-02-24 23:06:45 +0100

    working app, write feature missing

--- bandcamp_scraper/app.py
+++ bandcamp_scraper/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from main import get_release
 
 def run_flask():
     app = Flask(__name__)   
 
     @app.route("/")
-    
     def home():
-        return render_template("home.html") 
+        release = get_release() 
+        return render_template("home.html", release=release)
 
     app.run(host="0.0.0.0", port=8000, debug=True)
--- bandcamp_scraper/main.py
+++ bandcamp_scraper/main.py
@@ -1,11 +1,13 @@
 from sys import exit
 from email_scraper import create_database
-from app import run_flask
+import json
+from flask import Flask, render_template
 
 def find_database():
     try:
         with open("./database.json", "r") as file:
-            return file
+            database_json = json.load(file)
+            return database_json
     except FileNotFoundError:
         return False
 
@@ -14,6 +16,11 @@ def validate_mbox(path):
         return False
     return True
 
+def get_release(database): 
+    for item in database:
+        if item[2] == "0":
+            item[2] = "1"
+            return item[1]
 
 def main():
  
@@ -23,11 +30,18 @@ def main():
         mbox_path = input("Please enter path to valid .mbox file: ")
         if not validate_mbox(mbox_path):
             exit("Please enter valid mbox file")
-        database = create_database(mbox_path)
-        if not database:
+        if not create_database(mbox_path):
             exit("Could not create database")
+        database = find_database()
     
-    run_flask()
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        release = get_release(database)
+        return render_template("home.html", release=release)
+
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
 
 if __name__ == "__main__":
