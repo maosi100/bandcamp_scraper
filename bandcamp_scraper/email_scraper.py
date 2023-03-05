451e0d7381ca8d137cf9b49c526240eafbf2067f
Maxim Osipovs <maximosipovs@MacBook-Air-von-Maxim.local>
2023-03-05 17:35:37 +0100

    wip: count

--- bandcamp_scraper/email_scraper.py
+++ bandcamp_scraper/email_scraper.py
@@ -10,15 +10,20 @@ from datetime import datetime
 def make_EmailMessage(file):
     return email.message_from_binary_file(file, policy=policy.default)
 
+def add_increment(file):
+    for i, item in enumerate(file, 1):
+        item["Count"] = i
+    return True
+
+# Set up the mailbox using the factory above
 def create_database(path):
-    # Set up the mailbox using the factory above
     try:
         mbox = mailbox.mbox(path, factory=make_EmailMessage)
     except IsADirectoryError:
         return False
     
+    i = 0
     database = []
-
     for mail in mbox:
         if "release" in mail["Subject"]:
             input = mail.get_body(preferencelist=('html'))
@@ -30,9 +35,10 @@ def create_database(path):
             if "just released" in input:
                 if match := search(r"<a href=\"(.+)\">", str(input)):
                     url = match.groups(1)
-                    database.append({"Date": mail["Date"], "Url": url[0], "Flag": "0"})
+                    database.append({"Count": 1, "Date": mail["Date"], "Url": url[0], "Flag": "0"})
                     database.sort(key=lambda x: datetime.strptime(x["Date"], "%a, %d %b %Y %H:%M:%S %z"))
-
+    
+    add_increment(database)
     json_object = json.dumps(database, indent=4)
 
     with open("database.json", "w") as outfile:
