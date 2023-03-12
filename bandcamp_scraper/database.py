1b71148a20fd12d86bd398ecd68c76f5706e3086
Maxim Osipovs <maximosipovs@MacBook-Air-von-Maxim.local>
2023-03-12 22:09:08 +0100

    creating of Database class

---
+++ bandcamp_scraper/database.py
@@ -0,0 +1,49 @@
+from email_scraper import create_database
+import json
+from sys import exit
+
+class Database:
+
+    def __init__(self, filepath=None):
+        if self.validate_filepath(filepath):
+            create_database(filepath)
+
+        self.database = self.open_database()
+        self.length = self.database[-1]["Count"]
+
+    @staticmethod
+    def open_database():
+        try:
+            with open("./database.json", "r") as file:
+                database_json = json.load(file)
+                return database_json
+        except FileNotFoundError:
+            exit("Could not find database.json")
+
+    @staticmethod
+    def validate_filepath(filepath):
+        if "mbox" not in filepath[:-4]:
+            raise ValueError("Could not retrieve .mbox file")
+        return True
+
+    def get_release(self): 
+        for item in self.database:
+            if item["Flag"] == "0":
+                item["Flag"] = "1"
+                return item["Url"], item["Count"]
+
+    def save_state(self):
+        try:
+            with open("./database.json", "w") as file:
+                database = json.dumps(self.database, indent=4)
+                file.write(database)
+                return True
+        except FileNotFoundError:
+            print("Could not write database file")
+
+    def set_back(self):
+        for item in self.database:
+            if item["Flag"] == "0":
+                count = item["Count"] - 1
+                self.database[(count - 1)]["Flag"] = "0"
+                self.database[(count - 2)]["Flag"] = "0"
--- bandcamp_scraper/main.py
+++ bandcamp_scraper/main.py
@@ -1,73 +1,11 @@
-from email_scraper import create_database
-import json
 from flask import Flask, render_template, request
-
-# Retrieve database file
-def retrieve_database():
-    while True:
-        mbox_path = input("Please enter path to valid .mbox file: ")
-        if not validate_mbox(mbox_path):
-            print ("File invalid, try again.")
-        else:
-            break
-    if not create_database(mbox_path):
-        exit("Could not create database")
-
-# Open database
-def open_database():
-    try:
-        with open("./database.json", "r") as file:
-            database_json = json.load(file)
-            return database_json
-    except FileNotFoundError:
-        return False
-
-# Check for correct path
-def validate_mbox(path):
-    if "mbox" not in path[:-4]:
-        return False
-    return True
-
-# Get latest release from database
-def get_release(database): 
-    for item in database:
-        if item["Flag"] == "0":
-            item["Flag"] = "1"
-            return item["Url"], item["Count"]
-    return False
-
-# Save changes to database file
-def save_state(database):
-    try:
-        with open("./database.json", "w") as file:
-            database = json.dumps(database, indent=4)
-            file.write(database)
-            return True
-    except FileNotFoundError:
-        return False
-
-# Get the item count of database
-def count_items(database):
-    return database[-1]["Count"]
-
-# Set back database by two
-def set_back(database):
-    for item in database:
-        if item["Flag"] == "0":
-            count = item["Count"] - 1
-            database[(count - 1)]["Flag"] = "0"
-            database[(count - 2)]["Flag"] = "0"
-            return True
-
+from database import Database
 
 def main():
     # Access the release database
-    database = open_database()
-    if not database:
-        retrieve_database()
-        database = open_database()
+    database = Database("/Users/maximosipovs/Downloads/alte mailbox/Bandcamp all.mbox/mbox")
    
-    overall = count_items(database)
+    overall = database.length
 
     # Start Flask for application front end
     app = Flask(__name__)
@@ -75,7 +13,7 @@ def main():
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "GET":
-            return_values = get_release(database)
+            return_values = database.get_release()
             release = return_values[0]
             count = return_values[1]
             
@@ -85,8 +23,8 @@ def main():
                 return render_template("exceeded.html")
 
         if request.method == "POST":
-            set_back(database)
-            return_values = get_release(database)
+            database.set_back()
+            return_values = database.get_release()
             release = return_values[0]
             count = return_values[1]
             
@@ -97,7 +35,7 @@ def main():
 
     @app.route("/leave")
     def leave():
-        save_state(database)
+        database.save_state()
         return render_template("leave.html")
 
     app.run(host="0.0.0.0", port=8000, debug=True)
