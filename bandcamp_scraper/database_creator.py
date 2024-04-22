d3dbdfc5c5528ed9a6bc0ff07ef6c934e873b8a0
Maxim Osipovs <owsipovs@hotmail.de>
2024-04-22 20:55:23 +0200

    removed mailbox creator, housekeeping

--- bandcamp_scraper/database_creator.py
+++ bandcamp_scraper/database_creator.py
@@ -12,10 +12,10 @@ class DatabaseCreator:
     def __init__(self, filepath: str) -> None:
         self.mailbox = self.create_mailbox(filepath)
         self.database = list()
-        self._append_entry_to_database()
+        self._append_entry_to_database(self.mailbox)
         self.database.sort(key=lambda x: datetime.strptime(x["Date"], "%a, %d %b %Y %H:%M:%S %z"))
         self._write_json_from_list(self.database)
-
+    
     def create_mailbox(self, path: str) -> mailbox.mbox:
         try:
             return mailbox.mbox(path, factory=self._factory_EmailMessage)
@@ -25,23 +25,25 @@ class DatabaseCreator:
     @staticmethod
     def _factory_EmailMessage(file):
         return email.message_from_binary_file(file, policy=policy.default)
-
-    def _append_entry_to_database(self) -> None:
+    
+    def _append_entry_to_database(self, mailbox: mailbox.mbox) -> None:
         count = 1
-        for mail in self.mailbox:
+        for mail in mailbox:
             if "release" in mail["Subject"]:
                 mail_body = mail.get_body(preferencelist=('html')).as_string()
                 if url := self._extract_URL(mail_body):
                     self.database.append({"Count": count, "Date": mail["Date"], "Url": url, "Flag": "0"})
                     count += 1
-
-    def _write_json_from_list(self, database: list[Dict]) -> None:
+    
+    @staticmethod
+    def _write_json_from_list(database: list[Dict]) -> None:
         json_object = json.dumps(database, indent=4)
 
         with open("database.json", "w") as outfile:
             outfile.write(json_object)
 
-    def _extract_URL(self, mail: str) -> Union[str, None]:
+    @staticmethod
+    def _extract_URL(mail: str) -> Union[str, None]:
         if "base64" in mail:
             mail = Base64Decoder.decode_email(mail)
         if "iso-8859-1" in mail:
