c18224b91737875774e0c178df1ce4a06fe5e9ba
Maxim Osipovs <owsipovs@hotmail.de>
2024-04-24 17:37:51 +0200

    Removed ABC from EmailDecoder, housekeeping

--- bandcamp_scraper/database_creator.py
+++ bandcamp_scraper/database_creator.py
@@ -1,21 +1,22 @@
 import mailbox
 import email
-from email import policy
 import json
+
+from email import policy
 from re import search
 from datetime import datetime
 from typing import Union, Dict
 
-from email_decoder import Base64Decoder, Iso88591Decoder
+from email_decoder import EmailDecoder
 
 class DatabaseCreator:
     def __init__(self, filepath: str) -> None:
         self.mailbox = self.create_mailbox(filepath)
         self.database = list()
-        self._append_entry_to_database(self.mailbox)
+        self.append_entry_to_database()
         self.database.sort(key=lambda x: datetime.strptime(x["Date"], "%a, %d %b %Y %H:%M:%S %z"))
-        self._add_counts()
-        self._write_json_from_list(self.database)
+        self.add_counts()
+        self.write_json_from_list(self.database)
     
     def create_mailbox(self, path: str) -> mailbox.mbox:
         try:
@@ -27,31 +28,31 @@ class DatabaseCreator:
     def _factory_EmailMessage(file):
         return email.message_from_binary_file(file, policy=policy.default)
     
-    def _append_entry_to_database(self, mailbox: mailbox.mbox) -> None:
-        for mail in mailbox:
+    def append_entry_to_database(self) -> None:
+        for mail in self.mailbox:
             if "release" in mail["Subject"]:
                 mail_body = mail.get_body(preferencelist=('html')).as_string()
                 if url := self._extract_URL(mail_body):
                     self.database.append({"Count": 0, "Date": mail["Date"], "Url": url, "Flag": "0"})
-    
-    @staticmethod
-    def _write_json_from_list(database: list[Dict]) -> None:
-        json_object = json.dumps(database, indent=4)
-
-        with open("database.json", "w") as outfile:
-            outfile.write(json_object)
 
     @staticmethod
     def _extract_URL(mail: str) -> Union[str, None]:
         if "base64" in mail:
-            mail = Base64Decoder.decode_email(mail)
+            mail = EmailDecoder.decode_base64(mail)
         if "iso-8859-1" in mail:
-            mail = Iso88591Decoder.decode_email(mail)
+            mail = EmailDecoder.decode_iso8859(mail)
         
         if "just released" in mail:
             if match := search(r'<a href="([^"]+)">', mail):
                 return match.group(1)
 
-    def _add_counts(self) -> None:
+    def add_counts(self) -> None:
         for i, item in enumerate(self.database, 1):
             item["Count"] = i
+
+    @staticmethod
+    def write_json_from_list(database: list[Dict]) -> None:
+        json_object = json.dumps(database, indent=4)
+
+        with open("database.json", "w") as outfile:
+            outfile.write(json_object)
